7f0b80d490d1e6282bff6f7687dd24b897de2971
rinc <vadim@rinc.site>
2020-10-16 02:30:01 +0500

    Определение моделей и полей

--- core/surveys/models.py
+++ core/surveys/models.py
@@ -1,3 +1,225 @@
+from datetime import date
 from django.db import models
 
-# Create your models here.
+
+class Survey(models.Model):
+    id = models.AutoField(
+        primary_key=True, db_column='survey_id'
+    )
+
+    name = models.CharField(
+        verbose_name='Название опроса', max_length=127,
+        help_text='Например: Опрос о качестве услуг',
+        null=False, blank=True, default='Опрос',
+        db_column='survey_name'
+    )
+
+    start_date = models.DateField(
+        verbose_name="Дата начала",
+        editable=False,
+        null=False, blank=True, default=date.today(),
+        db_column='survey_start_date'
+    )
+
+    end_date = models.DateField(
+        verbose_name="Дата окончания",
+        null=False, blank=False, default=None,
+        db_column='survey_end_date'
+    )
+
+    description = models.TextField(
+        verbose_name='Описание', max_length=1024,
+        help_text='Например: Цель опроса -- получить оценку работы сотрудников Компании и стать лучше.',
+        null=True, blank=True, default=None,
+        db_column='survey_description'
+    )
+
+    class Meta:
+        ordering = ['start_date', 'name']
+        db_table = 'survey'
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+
+
+# class QuestionType(models.Model):
+#     id = models.AutoField(
+#         primary_key=True,
+#         db_column='question_type_id'
+#     )
+#
+#     name = models.CharField(
+#         db_column='question_type_name'
+#     )
+#
+#     description = models.CharField(
+#         db_column='question_type_description'
+#     )
+#
+#     class Meta:
+#         ordering = ['description']
+#         db_table = 'question_type'
+#         verbose_name = 'Тип вопроса'
+#         verbose_name_plural = 'Типы вопросов'
+
+
+class Question(models.Model):
+    id = models.AutoField(
+        primary_key=True,
+        db_column='question_id'
+    )
+
+    survey = models.ForeignKey(
+        verbose_name='Опрос',
+        to=Survey, on_delete=models.CASCADE,
+        related_name='questions',
+        null=False, blank=False, default=None,
+        db_column='survey_id'
+    )
+
+    QUESTION_TYPE_CHOICES = [
+        ('text', 'Ответ текстом'),
+        ('radio', 'Ответ с выбором одного варианта'),
+        ('checkbox', 'Ответ с выбором нескольких вариантов')
+    ]
+
+    type = models.CharField(
+        verbose_name='Тип ответа на вопрос', max_length=8,
+        help_text='Например: radio',
+        choices=QUESTION_TYPE_CHOICES,
+        null=False, blank=False, default='text',
+        db_column='question_type'
+    )
+
+    text = models.TextField(
+        verbose_name='Текст вопроса', max_length=127,
+        help_text='Например: Есть ли у Вас замечания?',
+        null=False, blank=False, default=None,
+        db_column='question_text'
+    )
+
+    class Meta:
+        ordering = ['survey', 'id']
+        db_table = 'question'
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+
+class QuestionOption(models.Model):
+    id = models.AutoField(
+        primary_key=True,
+        db_column='question_option_id'
+    )
+
+    question = models.ForeignKey(
+        verbose_name='Вопрос',
+        to=Question, on_delete=models.CASCADE,
+        related_name='question_options',
+        null=False, blank=False, default=None,
+        db_column='question_id'
+    )
+
+    text = models.TextField(
+        verbose_name='Текст варианта ответа', max_length=127,
+        help_text='Например: Скорее положительно',
+        null=False, blank=False, default=None,
+        db_column='question_option_text'
+    )
+
+    class Meta:
+        ordering = ['question', 'id']
+        db_table = 'question_option'
+        verbose_name = 'Вариант ответа'
+        verbose_name_plural = 'Варианты ответа'
+
+
+class Response(models.Model):
+    id = models.AutoField(
+        primary_key=True,
+        db_column='response_id'
+    )
+
+    survey = models.ForeignKey(
+        verbose_name='Опрос',
+        to=Survey, on_delete=models.CASCADE,
+        related_name='responses',
+        null=False, blank=False, default=None,
+        db_column='survey_id'
+    )
+
+    user_id = models.AutoField(
+        verbose_name='Идентификатор анонимного пользователя',
+        help_text='Например: 1234',
+        null=False, blank=False, default=None,
+        db_column='response_user_id'
+    )
+
+    class Meta:
+        ordering = ['survey', 'id']
+        db_table = 'response'
+        verbose_name = 'Результат'
+        verbose_name_plural = 'Результаты'
+
+
+class Answer(models.Model):
+    id = models.AutoField(
+        primary_key=True,
+        db_column='answer_id'
+    )
+
+    response = models.ForeignKey(
+        verbose_name='Результат',
+        to=Response, on_delete=models.CASCADE,
+        related_name='answers',
+        null=False, blank=False, default=None,
+        db_column='response_id'
+    )
+
+    question = models.ForeignKey(
+        verbose_name='Вопрос',
+        to=Question, on_delete=models.CASCADE,
+        related_name='answer',
+        null=False, blank=False, default=None,
+        db_column='question_id'
+    )
+
+    question_option = models.ForeignKey(
+        verbose_name='Вариант ответа',
+        to=QuestionOption, on_delete=models.CASCADE,
+        related_name='',
+        null=True, blank=True, default=None,
+        db_column='question_option_id'
+    )
+
+    text = models.TextField(
+        verbose_name='Текст ответа', max_length=255,
+        help_text='Например: Замечаний нет',
+        null=True, blank=True, default=None,
+        db_column='answer_text'
+    )
+
+    class Meta:
+        ordering = ['response', 'question']
+        db_table = 'answer'
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+
+
+# class AnswerOption(models.Model):
+#     id = models.AutoField(
+#         primary_key=True,
+#         db_column='answer_option_id'
+#     )
+#
+#     answer = models.ForeignKey(
+#         verbose_name='Ответ',
+#         to=Answer, on_delete=models.CASCADE,
+#         related_name='',
+#         null=False, blank=False, default=None,
+#         db_column='answer_id'
+#     )
+#
+#     class Meta:
+#         ordering = ['answer', 'id']
+#         db_table = 'answer_option'
+#         verbose_name = 'Выбранный вариант ответа'
+#         verbose_name_plural = 'Выбранные варианты ответа'
